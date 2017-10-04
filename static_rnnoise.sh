21d810fc31008a7e341d73fe89f03398ec1c2ba1
Luciano Dato <lucianodato@gmail.com>
2017-10-04 18:22:21 -0300

    Use github mirror for rnnoise

--- static_rnnoise.sh
+++ static_rnnoise.sh
@@ -2,13 +2,12 @@
 if [ -d "rnnoise" ]; then
   rm -rf ./rnnoise
 fi
-git clone https://git.xiph.org/rnnoise.git
+git clone https://github.com/xiph/rnnoise.git
 cd rnnoise/
 ./autogen.sh
 CFLAGS="-fvisibility=hidden -fPIC -Wl,--exclude-libs,ALL" \
 	./configure \
   --disable-examples --disable-doc \
-	#--enable-single --enable-sse --enable-avx --disable-mpi \
-	#--disable-shared --enable-static
-  -enable-sse --enable-avx 
+	--enable-single --enable-sse --enable-avx --disable-mpi \
+	--disable-shared --enable-static
 make -j2
