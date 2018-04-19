2c105a4d587507eb031134161763a966b51532aa
Luciano <lucianodato@gmail.com>
2018-04-19 10:42:45 -0300

    In Linux --exclude-libs is needed.

--- static_rnnoise.sh
+++ static_rnnoise.sh
@@ -1,13 +1,30 @@
-#!/bin/sh
-if [ -d "rnnoise" ]; then
-  rm -rf ./rnnoise
-fi
+#!/usr/bin/env bash
+
+rm -rf rnnoise || true
 git clone https://github.com/xiph/rnnoise.git
 cd rnnoise/
 ./autogen.sh
-CFLAGS="-fvisibility=hidden -fPIC " \ 
+
+if [ "$(uname)" == "Darwin" ]; then
+  CFLAGS="-fvisibility=hidden -fPIC " \ 
 	./configure \
   --disable-examples --disable-doc \
-	--disable-shared --enable-static
+	--disable-shared --enable-static        
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  CFLAGS="-fvisibility=hidden -fPIC -Wl,--exclude-libs,ALL" \
+	./configure \
+  --disable-examples --disable-doc \
+  --disable-shared --enable-static
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+#     CFLAGS="-fvisibility=hidden -fPIC -Wl,--exclude-libs,ALL" \
+# 	./configure \
+#   --disable-examples --disable-doc \
+#   --disable-shared --enable-static
+# elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+#     CFLAGS="-fvisibility=hidden -fPIC -Wl,--exclude-libs,ALL" \
+# 	./configure \
+#   --disable-examples --disable-doc \
+#   --disable-shared --enable-static
+fi
+
 make -j2
-#-Wl,--exclude-libs,ALL
